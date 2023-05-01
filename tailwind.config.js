12f0b44abc4b3fba1a2a031599755725d5404bd1
ArnaudD <arnaud.dumoncel@gmail.com>
2023-05-01 14:25:50 +0200

    import fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,17 @@
 /** @type {import('tailwindcss').Config} */
+let defaultTheme = require("tailwindcss/defaultTheme")
 export default {
   content: [
     "./index.html",
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Roboto', ...defaultTheme.fontFamily.sans],
+        mono: ['Inter', ...defaultTheme.fontFamily.mono]
+      }
+    },
   },
   plugins: [],
 }
