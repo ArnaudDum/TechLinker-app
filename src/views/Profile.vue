8617b9c60f2c9b2372b06ea13060a11b31242340
ArnaudD <arnaud.dumoncel@gmail.com>
2023-06-09 13:24:57 +0200

    style page profile

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -9,67 +9,96 @@
     <div class="bg-gray-dark">
       <div class="max-w-[1200px] mx-auto px-[30px] pb-[30px]">
         <div class="flex items-start gap-5 border-b-2 border-gray">
-          <div class="profile-pic rounded-full h-12 w-12">
+          <div class="profile-pic rounded-full h-12 w-12 relative">
             <img src="" alt="">
+            <button v-if="authStore.isAuthenticated" class="absolute bottom-2 right-1 cursor-pointer">
+              <font-awesome-icon icon="fa-solid fa-camera" />
+            </button>
           </div>
-          <div class="pt-5">
+          <div class="pt-5 relative">
+            <button v-if="authStore.isAuthenticated" class="absolute bottom-0 right-0 cursor-pointer">
+              <font-awesome-icon icon="fa-solid fa-pen-to-square" />
+            </button>
             <h1 class="font-mono text-2xl">Arnaud Dumoncel</h1>
             <p>Développeur Web</p>
           </div>
         </div>
-        <div class="py-8 border-b-2 border-gray">
+        <div class="py-8 border-b-2 border-gray relative">
+          <button v-if="authStore.isAuthenticated" class="absolute top-8 right-0 cursor-pointer">
+            <font-awesome-icon icon="fa-solid fa-pen-to-square" />
+          </button>
           <h2 class="profile-section-title font-mono text-2xl">Informations</h2>
-          <div class="my-8">
-            <h3 class="text-lg text-[#F3F3F380] mb-3">Localisation</h3>
-            <p>Rouen (76)</p>
-          </div>
-          <div>
-            <h3 class="text-lg text-[#F3F3F380] mb-3">Bio</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam</p>
+          <div class="py-8 grid grid-cols-1 md:grid-cols-2 gap-8">
+            <div>
+              <h3 class="text-lg text-[#F3F3F380] mb-3">Localisation</h3>
+              <p>Rouen (76)</p>
+            </div>
+            <div>
+              <h3 class="text-lg text-[#F3F3F380] mb-3">Bio</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam</p>
+            </div>
+            <div>
+              <h3 class="text-lg text-[#F3F3F380] mb-3">Liens</h3>
+              <ul>
+                <li><font-awesome-icon icon="fa-solid fa-globe" class="text-green me-2"/>Site personnel</li>
+                <li><font-awesome-icon icon="fa-brands fa-github" class="text-green me-2" />GitHub</li>
+                <li><font-awesome-icon icon="fa-brands fa-linkedin-in" class="text-green me-2" />LinkedIn</li>
+                <li><font-awesome-icon icon="fa-brands fa-dribbble" class="text-green me-2" />Dribbble</li>
+              </ul>
+            </div>
           </div>
         </div>
-        <div class="py-8 border-b-2 border-gray">
+        <div class="py-8 border-b-2 border-gray relative">
+          <button v-if="authStore.isAuthenticated" class="absolute top-8 right-0 cursor-pointer">
+            <font-awesome-icon icon="fa-solid fa-pen-to-square" />
+          </button>
           <h2 class="profile-section-title font-mono text-2xl">Technologies & rôles</h2>
-          <div class="my-8">
-            <h3 class="text-lg text-[#F3F3F380] mb-3">Rôles</h3>
-            <ul>
-              <li><font-awesome-icon class="text-green me-2" icon="fa-solid fa-check" />Développement Fullstack</li>
-              <li><font-awesome-icon class="text-green me-2" icon="fa-solid fa-check" />Conception UX / UI</li>
-              <li><font-awesome-icon class="text-green me-2" icon="fa-solid fa-check" />Gestion de projet</li>
-            </ul>
-          </div>
-          <div class="my-8">
-            <h3 class="text-lg text-[#F3F3F380] mb-3">Stack</h3>
-            <ul>
-              <li><font-awesome-icon class="text-green me-2" icon="fa-solid fa-check" />Javascript</li>
-              <li><font-awesome-icon class="text-green me-2" icon="fa-solid fa-check" />Vue.js</li>
-              <li><font-awesome-icon class="text-green me-2" icon="fa-solid fa-check" />Node.js</li>
-              <li><font-awesome-icon class="text-green me-2" icon="fa-solid fa-check" />Express.js</li>
-              <li><font-awesome-icon class="text-green me-2" icon="fa-solid fa-check" />MongoDB</li>
-              <li><font-awesome-icon class="text-green me-2" icon="fa-solid fa-check" />Tailwind</li>
-              <li><font-awesome-icon class="text-green me-2" icon="fa-solid fa-check" />Sass</li>
-            </ul>
-          </div>
-          <div>
-            <h3 class="text-lg text-[#F3F3F380] mb-3">Outils</h3>
-            <ul>
-              <li><font-awesome-icon class="text-green me-2" icon="fa-solid fa-check" />Git & GitHub</li>
-              <li><font-awesome-icon class="text-green me-2" icon="fa-solid fa-check" />Postman</li>
-              <li><font-awesome-icon class="text-green me-2" icon="fa-solid fa-check" />Visual Studio Code</li>
-            </ul>
+          <div class="py-8 grid grid-cols-1 md:grid-cols-2 gap-8">
+            <div>
+              <h3 class="text-lg text-[#F3F3F380] mb-3">Rôles</h3>
+              <ul>
+                <li><font-awesome-icon class="text-green me-2" icon="fa-solid fa-check" />Développement Fullstack</li>
+                <li><font-awesome-icon class="text-green me-2" icon="fa-solid fa-check" />Conception UX / UI</li>
+                <li><font-awesome-icon class="text-green me-2" icon="fa-solid fa-check" />Gestion de projet</li>
+              </ul>
+            </div>
+            <div>
+              <h3 class="text-lg text-[#F3F3F380] mb-3">Stack</h3>
+              <ul>
+                <li><font-awesome-icon class="text-green me-2" icon="fa-solid fa-check" />Javascript</li>
+                <li><font-awesome-icon class="text-green me-2" icon="fa-solid fa-check" />Vue.js</li>
+                <li><font-awesome-icon class="text-green me-2" icon="fa-solid fa-check" />Node.js</li>
+                <li><font-awesome-icon class="text-green me-2" icon="fa-solid fa-check" />Express.js</li>
+                <li><font-awesome-icon class="text-green me-2" icon="fa-solid fa-check" />MongoDB</li>
+                <li><font-awesome-icon class="text-green me-2" icon="fa-solid fa-check" />Tailwind</li>
+                <li><font-awesome-icon class="text-green me-2" icon="fa-solid fa-check" />Sass</li>
+              </ul>
+            </div>
+            <div>
+              <h3 class="text-lg text-[#F3F3F380] mb-3">Outils</h3>
+              <ul>
+                <li><font-awesome-icon class="text-green me-2" icon="fa-solid fa-check" />Git & GitHub</li>
+                <li><font-awesome-icon class="text-green me-2" icon="fa-solid fa-check" />Postman</li>
+                <li><font-awesome-icon class="text-green me-2" icon="fa-solid fa-check" />Visual Studio Code</li>
+              </ul>
+            </div>
           </div>
         </div>
-        <div class="py-8">
+        <div class="py-8 relative">
+          <button v-if="authStore.isAuthenticated" class="absolute top-8 right-0 cursor-pointer">
+            <font-awesome-icon icon="fa-solid fa-pen-to-square" />
+          </button>
           <h2 class="profile-section-title font-mono text-2xl">Activité</h2>
-          <div class="my-8">
-            <h3 class="text-lg text-[#F3F3F380] mb-3">Projets</h3>
-            <p>3 projets terminés</p>
-            <p>2 projets en cours</p>
-          </div>
-          <div>
-            <h3 class="text-lg text-[#F3F3F380] mb-3">Présence dans la communauté</h3>
-            <p>17 comptes suivis par Arnaud</p>
-            <p>12 comptes suivent Arnaud</p>
+          <div class="py-8 grid grid-cols-1 md:grid-cols-2 gap-8">
+            <div>
+              <h3 class="text-lg text-[#F3F3F380] mb-3">Projets</h3>
+              <p>Participation dans 3 projets</p>
+            </div>
+            <div>
+              <h3 class="text-lg text-[#F3F3F380] mb-3">Présence dans la communauté</h3>
+              <p>17 comptes suivis par Arnaud</p>
+              <p>12 comptes suivent Arnaud</p>
+            </div>
           </div>
         </div>
       </div>
@@ -91,6 +120,7 @@
     transform: translateY(-30%);
   }
 
+  h1,
   .profile-section-title {
     width: max-content;
     color: #6ECFA7;
@@ -102,7 +132,7 @@
 
   @media (min-width: 640px) {
     .profile-page {
-      top: 200px;
+      padding-top: 200px;
     }
   }
 </style>
